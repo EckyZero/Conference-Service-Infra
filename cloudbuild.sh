155608eb078de8f7c3cdcb323953a83f81b0d865
Erik Andersen <erik.andersen@parivedasolutions.com>
2020-06-20 15:47:14 -0500

    Initial commit
    - Separate out infrastructure from app database

---
+++ cloudbuild.sh
@@ -0,0 +1,22 @@
+APP_NAME=$1
+CONFIG_FILE=$2
+
+# Check if it already exists
+set +e
+O=`gcloud deployment-manager deployments describe ${APP_NAME} 2>&1`  
+exists=$?
+set -e
+
+if [ ${exists} -eq 0 ]; then
+    echo ${APP_NAME} exists
+    gcloud deployment-manager deployments update ${APP_NAME} --config=${CONFIG_FILE}
+else
+    # Enable Cloud SQL
+    gcloud services enable sqladmin.googleapis.com
+    
+    # Enable deployment manager
+    gcloud services enable deploymentmanager.googleapis.com
+    
+    # Run Deployment Manager
+    gcloud deployment-manager deployments create ${APP_NAME} --config=${CONFIG_FILE}
+fi
