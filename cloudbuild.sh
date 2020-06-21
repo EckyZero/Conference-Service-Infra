327167ad4c74baa4cc1eb47e296d83a714741977
Erik Andersen <erik.andersen@parivedasolutions.com>
2020-06-20 22:56:10 -0500

    - Enable secrets manager - Changed formatting to always revisit enabling new services on each push

--- cloudbuild.sh
+++ cloudbuild.sh
@@ -7,16 +7,20 @@ O=`gcloud deployment-manager deployments describe ${APP_NAME} 2>&1`
 exists=$?
 set -e
 
+# Enable Cloud SQL
+gcloud services enable sqladmin.googleapis.com
+
+# Enable deployment manager
+gcloud services enable deploymentmanager.googleapis.com
+
+# Enable deployment manager
+gcloud services enable secretmanager.googleapis.com
+    
+
 if [ ${exists} -eq 0 ]; then
     echo ${APP_NAME} exists
     gcloud deployment-manager deployments update ${APP_NAME} --config=${CONFIG_FILE}
 else
-    # Enable Cloud SQL
-    gcloud services enable sqladmin.googleapis.com
-    
-    # Enable deployment manager
-    gcloud services enable deploymentmanager.googleapis.com
-    
     # Run Deployment Manager
     gcloud deployment-manager deployments create ${APP_NAME} --config=${CONFIG_FILE}
 fi
